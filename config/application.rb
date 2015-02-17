4179f117f2bbab6ae0e43541b068370c3e3f22ba
Jessica <jbrazelt@thoughtworks.com>
2015-02-18 09:21:16 +1100

    Updated css and tried to fixed assets compiling on production.

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module RooksRealty
   class Application < Rails::Application
+  	config.assets.enabled = true
     config.assets.paths << Rails.root.join("vendor","assets","bower_components")
     config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
     config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff|woff2)$)
