be6a68d3444093b90e11ad8adf5e6a0acacb8b2b
Jessica <jbrazelt@thoughtworks.com>
2015-03-02 17:17:40 +1100

    Added cloud mailin.

--- app/mailers/emailer.rb
+++ app/mailers/emailer.rb
@@ -1,9 +1,9 @@
 class Emailer < ActionMailer::Base
-  default from: 'jbrazelton@knology.net'
+  default from: 'info@rooks-realty.com'
 
   def submit_contact_message(contact)
     @contact = contact
-    mail(:to => 'lynndupree06+wedding@gmail.com',
-         :subject => "Email from Contact Us Form")
+    mail(:to => 'lynndupree06@gmail.com, 7830a1ac067c675b34cc@cloudmailin.net',
+         :subject => "Email from Website")
   end
 end
