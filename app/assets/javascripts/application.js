221bd0d607804b00f9881ff90f84c0cd91d9654b
Jessica <jbrazelt@thoughtworks.com>
2015-03-05 23:00:32 +1100

    Fixed font-awesome asset problem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,5 @@
 //= require angular-resource/angular-resource
 //= require angular-route/angular-route
 //= require angular-bootstrap
-//= require ng-file-upload-shim
-//= require ng-file-upload
+//= require angular-file-upload
 //= require_tree .
