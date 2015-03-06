23205d7ae7acaba99be22658fa81cd854cd655d5
Jessica <jbrazelt@thoughtworks.com>
2015-03-07 04:27:49 +1100

    removed confirmable for user and used devise password mailer.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	devise :database_authenticatable, :registerable, :confirmable, :lockable, :timeoutable,
+	devise :database_authenticatable, :registerable, :lockable, :timeoutable,
          :recoverable, :rememberable, :trackable, :validatable
 
     after_create :send_reset_password_instructions
