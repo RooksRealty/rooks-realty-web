cd07a052f9fe2b58d81fe95feea8192908cb0d7e
Jessica <jbrazelt@thoughtworks.com>
2015-03-13 16:00:10 +1100

    reverted schedule showing javascript

---
+++ app/assets/javascripts/schedule-showing-controller.js
@@ -0,0 +1,29 @@
+var app = angular.module('schedule-showing', ['services', 'ui.bootstrap']);
+
+app.controller('ScheduleShowingController', ['$scope', '$modalInstance', 'listing', 'ShowingService',
+	function ($scope, $modalInstance, listing, ShowingService) {
+		$scope.listing = listing;
+		$scope.showAlert = false;
+		$scope.showOptions = false;
+		$scope.alert = { msg: '' };
+
+		$scope.cancel = function () {
+			$modalInstance.dismiss()
+		};
+
+		$scope.submit = function () {
+			if($scope.showingForm.$valid) {
+				$scope.showing.mls = $scope.listing.mls;
+				ShowingService.scheduleShowing({showing: $scope.showing}, function () {
+	              $modalInstance.close();
+	            }, function (error) {
+	              console.log(error);
+	            });
+			} else {
+				$scope.alert.msg = 'This form is not valid. Please fill in the required fields.';
+				$scope.showAlert = true;
+			}
+		};
+	}
+
+]);
