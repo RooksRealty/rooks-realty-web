ad3cead065c0ff782df796547d2096c6f5634650
Jessica <jbrazelt@thoughtworks.com>
2015-03-17 12:47:42 +1100

    Removed app constants to fix failing in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => AppConstants.sendgrid_username,
-  :password       => AppConstants.sendgrid_password,
+  :user_name      => 'app33555062@heroku.com',
+  :password       => '2gkgymxx',
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
--- config/initializers/load_app_constants.rb
+++ config/initializers/load_app_constants.rb
@@ -1,3 +1,4 @@
+require 'app_constants'
 # Loads the default configuration file located under RAILS_ROOT/config/constants.yml
 # Change the default location by modifying the following line:
 AppConstants.config_path = "#{Rails.root}/config/constants.yml"
